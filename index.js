f82b5fbc0c5a7251a9c1f950db2366998e2eed29
Om Raja <omraja451@gmail.com>
2024-08-30 21:24:31 +0530

    html code written, modified index.js but no express till now

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ import inquirer from "inquirer";
 import qr from "qr-image";
 import fs from "fs";
 
+let input = document.querySelector('input');
+let generateBtn = document.querySelector(".btn");
+const inputURL = "";
+generateBtn.addEventListener("click", (event)=>{
+  inputURL = input.value;
+});
+input.addEventListener("keydown", (event)=>{
+  if(event.key === "Enter"){
+    inputURL = input.value;
+  }
+});
+var qr_png = qr.image(inputURL); // default type = png
+qr_png.pipe(fs.createWriteStream('qr.png'));
+
+fs.writeFile("url.txt", url, (err)=>{
+  if(err) throw err;
+  console.log("File saved");
+})
+
+
+qr.image()
+
 inquirer
   .prompt([
     {message: "Paste a URL: ",
