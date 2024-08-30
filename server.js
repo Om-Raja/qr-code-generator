10ac165111feab1e7c9a1c964d86ac29c8614346
Om Raja <omraja451@gmail.com>
2024-08-31 03:21:48 +0530

    changed type module to commonjs to use __dirname & managed to send index.html on localhost:port using res.sendFile() method from server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-import express from "express";
+// import express from "express";
+const express = require("express");
 const app = express();
 
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + "/index.html");
+});
 
 const port = 3000;
 app.listen(port, ()=>{
